ba0992cc2fb92dd1d87340832f7d3ae15958f7e6
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-02-26 18:08:07 -0500

    made line and changed grid

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
 const stage = document.querySelector("#stage")
-
-
-import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
-kaboom({
-    width: +stage.style.width.value,
-    height: +stage.style.height.value,
-    canvas: stage,
-    background: [ 0, 0, 255, ],
-})
-
-
-document.addEventListener("HTMLContentLoaded", makeGrid())
-
-function makeGrid() {
-    const grid = []
-    let startX = 345
-    let startY = 250
-    for (let i = 0; i < 3; i++) {
-        startX = 345
-        startY += 100
-        grid.push([])
-        for (let j = 0; j < 6; j++) {
-            startX += 100
-             makeCell(startX, startY)
-            grid[i].push(0)
-        }
-    }
-    return grid
-}
-
-function makeCell (x, y) {
-    return add([
-        // sprite('cell'),
-        pos(x, y),
-        rect(60, 70),
-        outline(4),
-        area(),
-    ])
+const resumeButton = document.querySelector("#resume")
+const trackLine = {
+    element : document.createElement("div")
+    width :
 }
 
 
+document.addEventListener("HTMLContentLoaded", makeTrackLine())
+resumeButton.addEventListener("click", setInterval(moveTrackLine, 250))
 
 
+function makeTrackLine (){
+    const trackLine = document.createElement("div")
+    stage.appendChild(trackLine)
+    trackLine.style.width = "10px"
+    trackLine.style.height = "250px"
+    trackLine.style.backgroundColor = "white"
+    trackLine.style.position = "absolute"
+    return trackLine
+}
+
+function moveTrackLine (){
+    trackLine.style.left = 
+}
 
