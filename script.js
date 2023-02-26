3bdf3a58ba5a80ca1e96e5e9cc341f6a0301b852
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-02-26 18:38:20 -0500

    trying to make the line move

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const stage = document.querySelector("#stage")
 const resumeButton = document.querySelector("#resume")
-const trackLine = {
-    element : document.createElement("div")
-    width :
+const trackLine = makeTrackLine()
+stageWidth = stage.offsetWidth
+stageHeight = stage.offsetHeight
+
+
+resumeButton.addEventListener("click", () => setInterval(moveTrackLine, 250))
+
+function makeTrackLine() {
+    const gameItem = {
+        element: document.querySelector('#track-line'),
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+        directionX: 0,
+        directionY: 0,
+        speed: 10,
+    }
+    gameItem.width = gameItem.element.offsetWidth;
+    gameItem.height = gameItem.element.offsetHeight;
+    gameItem.element.style.position = "absolute";
+    stage.appendChild(gameItem.element);
+    return gameItem;
 }
 
-
-document.addEventListener("HTMLContentLoaded", makeTrackLine())
-resumeButton.addEventListener("click", setInterval(moveTrackLine, 250))
-
-
-function makeTrackLine (){
-    const trackLine = document.createElement("div")
-    stage.appendChild(trackLine)
-    trackLine.style.width = "10px"
-    trackLine.style.height = "250px"
-    trackLine.style.backgroundColor = "white"
-    trackLine.style.position = "absolute"
-    return trackLine
+function moveTrackLine() {
+    trackLine.x += trackLine.directionX * trackLine.speed;
+    trackLine.y += trackLine.directionY * trackLine.speed;
+    trackLine.element.style.left = `${trackLine.x}px`;
+    trackLine.element.style.top = `${trackLine.y}px`;
 }
-
-function moveTrackLine (){
-    trackLine.style.left = 
-}
-
