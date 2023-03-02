624ecaf82578e5f83a8356154c0b5caab80a1755
Afairman2003 <afairman@brooklynsteamcenter.org>
2023-03-02 01:48:58 +0000

    completed

---
+++ button.js
@@ -0,0 +1,18 @@
+// let button = document.getElementByClassName("sound");
+// button.addEventListener("click",alert)
+
+// function alert(){
+//     console.log("hi")
+// //   new Audio("./sounds/clickSound.mp3").play()
+// }
+
+function playAudio(){
+    new Audio("./sounds/clickSound.mp3").play()
+}
+
+
+
+// function playSong(){
+//     new Audio("./sounds/Trance.mp3").play();
+// }
+// playSong();
