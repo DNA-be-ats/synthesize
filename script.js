907de7b54b3ce0e63472c59f8ef5eff59472dc31
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-03-02 00:35:03 -0500

    got Beat Pad MVP

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const stage = document.querySelector("#stage")
-const resumeButton = document.querySelector("#resume")
+const sampleRowCells = Array.from(document.getElementsByClassName("sample-sound"))
+const playButton = document.querySelector("#play")
+const resetButton = document.querySelector("#reset")
 const trackLine = setUpTrackLine()
 const atTheReady = setupCells()
 const allRows = document.querySelectorAll(".row")
 const BPMButton = document.querySelector("#BPM")
 const BPMinput = document.querySelector("#BPMinput")
-const possibleCellSounds = [new Audio('audio-assets/!benjamino [808] @typical1k.wav'),new Audio('audio-assets/Crash (Zay) (2).wav'), new Audio('audio-assets/Thin Clap @thecxdy.wav')]
-const possibleCellColors = ["#970380","#2e1258","#fae273"]
-
+const possibleCellSounds = [new Audio('audio-assets/!anotherdownohat [ohat] @typical1k.wav'),new Audio('audio-assets/CB_Clap.wav'), new Audio('audio-assets/CB_Hat.wav'),new Audio("audio-assets/CB_Kick.wav"), new Audio("audio-assets/CB_snare.wav")]
+const possibleCellColors = ["#970380","#2e1258","#fae273",'red','blue']
+const form = document.querySelector("#form")
+let rowCounter = 0
 let currBPM = 120
+let convertedBPM = 1000/(currBPM/60)
 
 
 
@@ -18,24 +22,37 @@ let currBPM = 120
 stageWidth = stage.offsetWidth
 stageHeight = stage.offsetHeight
 setTrackLinePosition()
+setUpSampleRowCells()
 
 
+resetButton.addEventListener("click",resetAllCells)
+form.addEventListener('submit', changeBPM)
+playButton.addEventListener("click", (e) => {
+    const target = e.target
+    const bpmInterval = setInterval(playActiveCells, convertedBPM)
+    if(target.id === 'play'){
+        e.target.id = 'resume'
+        e.target.textContent = 'Resume'
+        moveTrackLine()
+    } else {
+        e.target.id = 'play'
+        e.target.textContent = 'Play'
 
-resumeButton.addEventListener("click", moveTrackLine)
+    }
+})
 atTheReady.forEach(cell => {
-    // cell.element.addEventListener('dbclick', (e) => {
-    //     console.log(e)
-    //     const clickedCell = e.target
-    //     let clickedCellStyle = clickedCell.style
-    //     clickedCellStyle.backgroundColor = "red"
-    // })
     cell.element.addEventListener('click', changeCellSate)
 })
 
-// BPMinput.addEventListener("submit", (e) =>{
-//     e.preventDefault()
-//     console.log(e)
-// })
+function setUpSampleRowCells(){
+    for(let i = 0; i < sampleRowCells.length; i++){
+        const currCell = sampleRowCells[i]
+        currCell.style.backgroundColor = possibleCellColors[i]
+        currCell.addEventListener('click', () => {
+            possibleCellSounds[sampleRowCells.indexOf(currCell)].play()
+        })
+    }
+}
 
 function setupCells () {
     const collectedCells = document.querySelectorAll(".cell")
@@ -55,6 +72,7 @@ function setupCells () {
 
 function setUpTrackLine() {
     const gameItem = document.querySelector('#track-line')
+    console.log(gameItem.getBoundingClientRect().left)
     gameItem.width = gameItem.offsetWidth;
     gameItem.height = gameItem.offsetHeight;
     gameItem.style.position = "absolute";
@@ -64,77 +82,63 @@ function setUpTrackLine() {
  function moveTrackLine() {
     setTrackLinePosition()
     let currPositionX = trackLine.getBoundingClientRect().left
-    const anim = setInterval(animate,10)
+    const anim = setInterval(animate,convertedBPM/80)
     function animate() {
-        isTouchingRow ()
         currPositionX++
-        const newPosition = currPositionX
-        if(newPosition >= stageWidth*1.25) clearInterval(anim)
-        trackLine.style.left = `${newPosition}px`;
+        const newPosition = currPositionX 
+        if(newPosition >= stageWidth*1.25) {
+            playButton.id = 'play'
+            playButton.textContent = 'Play'
+            clearInterval(anim)
+            setTrackLinePosition()
+        }else {
+            trackLine.style.left = `${newPosition}px`
+        }
     }
  }
 
-function setTrackLinePosition () {
+function setTrackLinePosition() {
     const row1 = document.querySelector("#row1")
-    trackLine.style.left = `${row1.getBoundingClientRect().left - 10}px`;
+    trackLine.style.left = `${row1.getBoundingClientRect().left - 2}px`;
 }
 
-function isTouchingRow () {
-    for(let i = 0; i < allRows.length; i++) {
-        const currRow = allRows[i]
-        if(trackLine.getBoundingClientRect().x + trackLine.offsetWidth > currRow.getBoundingClientRect().x && trackLine.getBoundingClientRect().x < currRow.getBoundingClientRect().x + currRow.offsetWidth){
-            playActiveCells(currRow)
-        } else{
-            currRow.classList.remove("active-row")
-        } 
+function playActiveCells() {
+    const currRow = allRows[rowCounter]
+    const childrenOfRow = Array.from(currRow.children)
+    childrenOfRow.forEach((cell,i) => {
+        if(cell.classList.contains("active-cell")){
+            possibleCellSounds[i].play()
+            console.log(possibleCellSounds[i])
+        }
+    })
+    rowCounter++
+    if(rowCounter >= allRows.length) {
+        rowCounter = 0
     }
 }
 
-function activeCells(row) {
-// RE-VISIT THIS ONCE YOU MADE CELLS CLICKABLE
-
-//     row.classList.add("active-row")
-//     row.forEach(cell => {
-//         cell.active = true
-//         cell.sound = new Audio(cell.element.dataset.sound)
-//         cell.sound.play()
-//     })
+function changeBPM(e) {
+    e.preventDefault()
+    currBPM = Number(e.target.children[1].value)
+    convertedBPM = 1000/(currBPM/60)
 }
 
-function playActiveCells(row) {
-    const childrenOfRow = Array.from(row.children)
-    childrenOfRow.forEach((cell,i) => {
-        //This could be useful ill look at it later
-        // if(cell.classList.contains("active-cell")) {
-        //     cell.classList.remove("active-cell")
-        //     cell.sound.pause()
-        //     cell.sound.currentTime = 0
-        // }
-        if(cell.classList.contains("active-cell")){
-            possibleCellSounds[i].play()
-        }
-    })
-}
 
-function changeBPM() {
-
-
-}
 /////////////////////////////////////////////
 ////Attempting Audio Import for Samples
 ////////////////////////////////////////////
-bass.addEventListener("click", function(e){
-    console.log(e)
-    const bassAudio = document.getElementById("sample-sound");
-    // const audioCtx = new AudioContext(); 
-    bassAudio.play();
-
-})
+// bass.addEventListener("click", function(e){
+//     console.log(e)
+//     const bassAudio = document.getElementById("sample-sound");
+//     // const audioCtx = new AudioContext(); 
+//     bassAudio.play()
 
 function changeCellSate(event) {
     const clickedCell = event.target
     let clickedCellStyle = clickedCell.style
-    if(clickedCell.classList.contains("active-cell")) {
+    if(clickedCell.classList.contains('sample-sound')) {
+        possibleCellSounds[sampleRowCells.indexOf(clickedCell)].play()
+    } else if(clickedCell.classList.contains("active-cell")) {
         clickedCell.classList.remove("active-cell")
         clickedCellStyle.backgroundColor = "black"
     } else {
@@ -144,20 +148,18 @@ function changeCellSate(event) {
     }
 }
 
-// function chooseCellColor(Cell) {
-    
-//     if(childrenOfRow.indexOf(clickedCell) === 0){
-//         clickedCellStyle.backgroundColor = "#970380"
-//     } else if(childrenOfRow.indexOf(clickedCell) === 1){
-//         clickedCellStyle.backgroundColor = "#2e1258"
-//     } else if(childrenOfRow.indexOf(clickedCell) === 2){
-//         clickedCellStyle.backgroundColor = "#fae273"
-//     } 
-// }
-// function rightclick() {
-//     const rightclick;
-//     const e = window.event;
-//     if (e.which) rightclick = (e.which == 3);
-//     else if (e.button) rightclick = (e.button == 2);
-//     alert(rightclick); // true or false, you can trap right click here by if comparison
-// }
+function resetAllCells() {
+    atTheReady.forEach(cell => {
+        cell.element.classList.remove("active-cell")
+        cell.element.style.backgroundColor = "black"
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", (e) => { 
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n");
+})
